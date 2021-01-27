fc1d1012024e254899161e26c4b7408dbdb57803
Pedro Leite <pedrobuenoleite@gmail.com>
2021-01-26 22:33:47 -0300

    Project Foundation use Spring Batch

---
+++ data-ingestion/src/main/java/com/example/dataingestion/domain/CityInput.java
@@ -0,0 +1,26 @@
+package com.example.dataingestion.domain;
+
+public class CityInput {
+
+    private String city;
+
+    private String cityAscii;
+
+    private String lat;
+
+    private String lng;
+
+    private String country;
+
+    private String iso2;
+
+    private String iso3;
+
+    private String adminName;
+
+    private String capital;
+
+    private String population;
+
+    private String id;
+}
